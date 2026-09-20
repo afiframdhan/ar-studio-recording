ea43b267efbb225db3f5f96c18a7f5a7a3458747
afiframdhan <afiframdhan@gmail.com>
2026-09-20 18:54:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Tempel URL deployment Google Apps Script yang berakhiran /exec di bawah ini.
+// Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+window.AR_STUDIO_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbxyB9FWKGDi-B9JQ8hfbqeSWZ8vyy_su0JhEJ9cKLJ_rSHaqJ9s5rktkOuhhAL-EO4uNg/exec'
+};
