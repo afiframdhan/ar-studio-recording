88e1ffb13170001f92519593699eda6653b572cd
afiframdhan <afiframdhan@gmail.com>
2026-09-20 20:53:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Konfigurasi AR Studio Recording
+window.AR_STUDIO_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbxyB9FWKGDi-B9JQ8hfbqeSWZ8vyy_su0JhEJ9cKLJ_rSHaqJ9s5rktkOuhhAL-EO4uNg/exec',
+  BASE_PATH: '/ar-studio-recording/',
+  CACHE_MINUTES: 10
+};
