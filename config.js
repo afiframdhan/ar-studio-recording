d97be4b49fedfb472003fb8cefedf26ab776347b
Afif Ramdhan <afiframdhan@gmail.com>
2026-09-24 19:30:15 +0700

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Konfigurasi AR Studio Recording
 window.AR_STUDIO_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxyB9FWKGDi-B9JQ8hfbqeSWZ8vyy_su0JhEJ9cKLJ_rSHaqJ9s5rktkOuhhAL-EO4uNg/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzlNZAeltlUX1fuRACLoVYVlIQ99dxSiBDE3eNVBDPcAkMTsAkIpaTnemlo2RvVNweC/exec',
   BASE_PATH: '/ar-studio-recording/',
   CACHE_MINUTES: 2
 };
